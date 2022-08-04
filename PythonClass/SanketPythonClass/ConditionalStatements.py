bbb5e294a39f81ec518be6476de3177a9fb35ce8
aadesh lokhande <aadeshlokhande11@gmail.com>
2022-08-04 08:51:36 +0530

    conditional statements

---
+++ PythonClass/SanketPythonClass/ConditionalStatements.py
@@ -0,0 +1,38 @@
+# conditional statements - decision making statements
+
+# *************if***********
+# if(condition):
+#     statements
+
+# age = int(input("enter your age = "))
+
+# if(age>18):
+#     print("Adult ")
+
+# print("boy")
+
+# ******************if-else************
+# if(condition):
+#     statements
+# else:
+#     statements
+
+# age = int(input("enter your age = "))
+# if(age>18):
+#     print("you can vote")
+# else:
+#     print("you can't vote")
+
+
+num = int(input("enter a number = "))
+if(num>0):
+    print("+ve number")
+else:
+    print("-ve number")
+
+# homework
+# get value from user and print the number is even or odd 
+# get year from user and check its leap year or not 
+# get 2 values from user and print greater number 
+# get 3 angles from user and check its triangle or not 
+
