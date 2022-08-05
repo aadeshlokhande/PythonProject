810f771909fae41e9cc469d3653d77e5fc7212c8
aadesh lokhande <aadeshlokhande11@gmail.com>
2022-08-05 09:00:53 +0530

    notes

--- PythonClass/SanketPythonClass/004 ConditionalStatements.py
+++ PythonClass/SanketPythonClass/004 ConditionalStatements.py
@@ -24,15 +24,29 @@
 #     print("you can't vote")
 
 
-num = int(input("enter a number = "))
-if(num>0):
-    print("+ve number")
-else:
-    print("-ve number")
+# num = int(input("enter a number = "))
+# if(num>0):
+#     print("+ve number")
+# else:
+#     print("-ve number")
 
 # homework
 # get value from user and print the number is even or odd 
-# get year from user and check its leap year or not 
+
+# num = int(input("enter a number = "))
+# if(num%2==0):
+#     print("even number")
+# else:
+#     print("odd number")
+
+# get year from user and check its leap year or not
+
+num = int(input("enter a number = "))
+if(num%4==0):
+    print("leap year")
+else:
+    print("not a leap year")
+
 # get 2 values from user and print greater number 
 # get 3 angles from user and check its triangle or not 
 
