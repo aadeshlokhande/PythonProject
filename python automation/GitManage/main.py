071d061dcd093dc3c1b1de498760f7c8c68a61da
aadesh lokhande <aadeshlokhande11@gmail.com>
2022-08-09 02:14:12 +0530

    {Date}

--- python automation/GitManage/main.py
+++ python automation/GitManage/main.py
@@ -1,6 +1,11 @@
 import os
+from datetime import date
+
 folders = ["Bvoc","codeshot","CPP\ Projects","CProjects","Python\ Projects","WebsiteCalculator"]
+
+today = date.today()
+Date = today.strftime("%d-%m-%Y")
 
 for folder in folders:
     os.system(f"cd /home/coder/Documents/MyProjects/{folder}")
-    os.system("git status && git add . && git commit -m \"none\" && git push -u origin main")
+    os.system('git status && git add . && git commit -m "{Date}" && git push -u origin main')
