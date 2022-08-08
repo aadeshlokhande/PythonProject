4c3380a6737f76fca608cfeea8efc6a5ae8c5685
aadesh lokhande <aadeshlokhande11@gmail.com>
2022-08-09 02:09:35 +0530

    none

--- python automation/GitManage/main.py
+++ python automation/GitManage/main.py
@@ -1,3 +1,6 @@
 import os
-os.system("cd /home/coder/Documents/MyProjects/Python\ Projects")
-os.system("git status && git add . && git commit -m \"none\" && git push -u origin main")
+folders = ["Bvoc","codeshot","CPP\ Projects","CProjects","Python\ Projects","WebsiteCalculator"]
+
+for folder in folders:
+    os.system(f"cd /home/coder/Documents/MyProjects/{folder}")
+    os.system("git status && git add . && git commit -m \"none\" && git push -u origin main")
