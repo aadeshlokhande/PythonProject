c1e3cd6bba284fdf742563f19e1106fde3ee520b
aadesh lokhande <aadeshlokhande11@gmail.com>
2022-08-23 18:57:51 +0530

    23-08-2022

---
+++ PythonClass/PythonSanketVedant/001basics.py
@@ -0,0 +1,37 @@
+# variables - cantainer
+# a = 10
+# a = 123123123
+# b = 1231231231
+# c = 23123124423
+# d = 42342342434
+# e = 324234234344
+
+# variable
+# vari - able 
+
+# a = 10
+# a = 20
+# a = 30
+# ========================================
+
+# data type 
+
+# int -   12,43,6,85,24,68,-34,-456
+# float - 3.14,34.5363, 15.5, 5.6,1.5
+# string - "aadesh",'aadesh','''aadesh''',"""aadesh"""
+
+# a = 29+1
+# print(a)
+# print(type(a))
+
+# b = 3.55+1
+# print(b)
+# print(type(b))
+
+
+
+# print(a+b)
+# c = "aadesh" + "lokhande"
+# print(c)
+# print(type(c))
+
