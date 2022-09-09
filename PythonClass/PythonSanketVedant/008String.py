e940ac5f8a0bc5f15ab09e205114a305fc521b3e
aadesh lokhande <aadeshlokhande11@gmail.com>
2022-09-09 17:02:50 +0530

    09-09-2022

--- PythonClass/PythonSanketVedant/008String.py
+++ PythonClass/PythonSanketVedant/008String.py
@@ -23,3 +23,44 @@
 # a = input("enter your name = ")
 # print(a)
 # print(type(a))
+
+# -------------- METHODS ----------
+# str = "Hello My Name is Aadesh Lokhande"
+# print(len(str))
+# print(type(str))
+# print(dir(str))
+# print(str.swapcase())
+# print(str.upper())
+# print(str.lower())
+# print(str.capitalize())
+# print(str.title())
+# print(str.find("is"))
+# print(str.count("e"))
+# print(str.isdigit())
+# print(str.join(["hello","bye bye","lol"]))
+# print(str.split("e"))
+
+
+
+# str = "12a"
+# print(str.isalnum())
+# print(str.isalpha())
+# print(str.isascii())
+# print(str.isidentifier())
+# print(str.islower())
+# print(str.isprintable())
+# print(str.islower())
+# print(str.isupper())
+
+
+# --------------format------------------
+# name = input("Enter your name = ")
+# age = int(input("Enter your age = "))
+# print("my name is",name,"and my age is",age,"year old")
+# print(f"my name is {name} and my age is {age} year old")
+
+# ----------------concadination---------------------
+# a = "hello "
+# b = "world "
+# c = a + b
+# print(c*10)
