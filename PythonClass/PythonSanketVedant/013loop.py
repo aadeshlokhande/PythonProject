9e417c34047737c65e50dc40bf5db7b3c52c5cd9
aadesh lokhande <aadeshlokhande11@gmail.com>
2022-09-14 16:59:39 +0530

    14-09-2022

---
+++ PythonClass/PythonSanketVedant/013loop.py
@@ -0,0 +1,28 @@
+# loop 
+# for 
+
+# for var in collectionData:
+#     statements
+
+# a = [1,2,3,4,5,10]
+# a = "aadesh lokhande"
+# for abc in a:
+#     print(abc)
+
+# Range()
+# for a in range(10):
+#     print(a)
+
+# for a in range(100):
+#     print("Sorry ",end="")
+
+# for a in range(1,11):
+#     print(a)
+
+# for a in range(10,-11,-1):
+#     print(a)
+
+
+# +++++++++++++HomeWork+++++++++++++
+# get number from user and print the table
+# print all even numbers from 0 to 100
