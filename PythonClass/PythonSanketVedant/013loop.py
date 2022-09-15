4645db47f5c3144bb5b9d0ad2309451fe0bf786b
aadesh lokhande <aadeshlokhande11@gmail.com>
2022-09-15 16:52:31 +0530

    15-09-2022

--- PythonClass/PythonSanketVedant/013loop.py
+++ PythonClass/PythonSanketVedant/013loop.py
@@ -25,4 +25,12 @@
 
 # +++++++++++++HomeWork+++++++++++++
 # get number from user and print the table
+
+# num = int(input("Enter a number = "))
+# for i in range(1,11):
+#     print(f"{num} x {i} = {i*num}")
+
 # print all even numbers from 0 to 100
+for i in range(101):
+    if(i%2==0):
+        print(i)
