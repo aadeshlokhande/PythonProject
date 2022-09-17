9e7efe09b33df0aa5e46833b2320cd4860266795
aadesh lokhande <aadeshlokhande11@gmail.com>
2022-09-17 16:58:02 +0530

    17-09-2022

--- PythonClass/PythonSanketVedant/014whileloop.py
+++ PythonClass/PythonSanketVedant/014whileloop.py
@@ -21,6 +21,15 @@
 # ******************Homework************
 # get 2 numbers from user and print addtion till answer is zero 
 
+# ans = 1
+# while(ans!=0):
+#     a = int(input("Enter a number = "))
+#     b = int(input("Enter a number = "))
+#     ans = a + b 
+#     print(f"{a} + {b} = {ans}")
+
+
+
 # get numbers from user till he enter zero... and when he enter zero print total sum of all number 
 
 # get values from user till he didn't enter negative number 
