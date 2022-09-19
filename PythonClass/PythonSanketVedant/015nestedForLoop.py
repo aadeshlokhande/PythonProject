56151008207039433605ad1b5581be125c0010b0
aadesh lokhande <aadeshlokhande11@gmail.com>
2022-09-19 17:02:29 +0530

    19-09-2022

---
+++ PythonClass/PythonSanketVedant/015nestedForLoop.py
@@ -0,0 +1,92 @@
+# Nested For Loop 
+# for variable in CollectionOfData:
+#     statements
+#     for variable in CollectionOfData:
+#         statements
+#     statements
+
+# for i in range(5):
+#     for j in range(5):
+#         print("*",end=" ")
+#     print()
+
+
+# for i in range(1,6):
+#     for j in range(1,6):
+#         print(i,j,end="\t")
+#     print()
+
+
+# for i in (1,2,3,4,5):
+#     for j in (1,2,3,4,5):
+#         print(j,end="\t")
+#     print()
+
+
+# for i in range(1,6):
+#     for j in range(i):
+#         print("*",end=" ")
+#     print()
+
+
+# for i in range(5,0,-1):
+#     for j in range(i):
+#         print("*",end=" ")
+#     print()
+
+
+# 1 1 1
+# 0 0 0
+# 0 0 0 
+
+
+# 11 12 13 
+# 21 22 23
+# 31 32 33
+
+
+# for i in range(1,4):
+#     for j in range(1,4):
+#         if(i==1):
+#             print(1,end=" ")
+#         else:
+#             print(0,end=" ")
+#     print()
+
+
+# 1 0 0 
+# 1 0 0 
+# 1 0 0 
+
+# 1 0 0 
+# 0 1 0 
+# 0 0 1
+
+# 1 0 0 
+# 1 1 0 
+# 1 1 1 
+
+# 1 1 1 
+# 0 1 1 
+# 0 0 1 
+
+# 1 0 1 
+# 0 1 0 
+# 1 0 1
+
+# 1 1 1 1 1 
+# 1 0 0 0 1 
+# 1 0 0 0 1 
+# 1 0 0 0 1
+# 1 1 1 1 1
+
+
+# 1 1 1 1 1 
+# 1 1 0 1 1 
+# 1 0 1 0 1 
+# 1 1 0 1 1
+# 1 1 1 1 1
+
+# 1 - 100 counting print 
+
+# 1 - 10 tables 
