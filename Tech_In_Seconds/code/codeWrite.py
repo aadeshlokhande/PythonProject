1117747a950ba08e51a8358820e0b5f19920b1e8
aadesh lokhande <aadeshlokhande11@gmail.com>
2022-09-20 00:43:51 +0530

    20-09-2022

--- Tech_In_Seconds/code/codeWrite.py
+++ Tech_In_Seconds/code/codeWrite.py
@@ -1,37 +1,20 @@
 import pyautogui as pg
 from time import sleep
 
-filename = "NoteSorting.c"
+filename = "addition.c"
 
 credit = "// Author : @Tech_In_Seconds\n"
 
 data = """#include <stdio.h>
-int main() 
+int main()
 {
-    int amt, total;
-    printf("Input the amount: ");
-    scanf("%d",&amt);
-    total = (int)amt/100;
-    printf("There are: \\n");
-    printf("%d Note(s) of 100.00\\n", total);
-    amt = amt-(total*100);
-    total = (int)amt/50;
-    printf("%d Note(s) of 50.00\\n", total);
-    amt = amt-(total*50);
-    total = (int)amt/20;
-    printf("%d Note(s) of 20.00\\n", total);
-    amt = amt-(total*20);
-    total = (int)amt/10;
-    printf("%d Note(s) of 10.00\\n", total);
-    amt = amt-(total*10);
-    total = (int)amt/5;
-    printf("%d Coin(s) of 5.00\\n", total);
-    amt = amt-(total*5);
-    total = (int)amt/2;
-    printf("%d Coin(s) of 2.00\\n", total);
-    amt = amt-(total*2);
-    total = (int)amt/1;
-    printf("%d Coin(s) of 1.00\\n", total);
+    int num1,num2,ans;
+    printf("Enter a first number = ");
+    scanf("%d",&num1);
+    printf("Enter a first number = ");
+    scanf("%d",&num2);
+    ans = num1 + num2;
+    printf("%d + %d = %d\\n",num1,num2,ans);
     return 0;
 }
 """
@@ -43,6 +26,7 @@ data = data.replace("    ","")
 sleep(1)
 pg.click(400,300)
 sleep(1)
+pg.moveTo(800,800)
 
 for i in data:
     if(i=="<"):
@@ -53,10 +37,16 @@ for i in data:
     else:
         pg.typewrite(i)
 
-sleep(2)
+sleep(1)
 
 
 pg.hotkey("alt","t")
 sleep(2)
-pg.typewrite(f"gcc {filename} && ./a.out",0.05)
+pg.typewrite(f"gcc {filename} && ./a.out",0.08)
+pg.press("Enter")
+
+pg.typewrite(f"10",0.2)
+pg.press("Enter")
+
+pg.typewrite(f"20",0.2)
 pg.press("Enter")
