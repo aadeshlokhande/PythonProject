b8284cf4e6b82cde09bae1df68060f3186b1a4f5
aadesh lokhande <aadeshlokhande11@gmail.com>
2022-09-22 18:30:26 +0530

    22-09-2022

--- PythonClass/PythonSanketVedant/015nestedForLoop.py
+++ PythonClass/PythonSanketVedant/015nestedForLoop.py
@@ -185,9 +185,25 @@
 #   * * * * 
 # * * * * *
 
+# for i in range(1,6):
+#     for j in range(1,6):
+#         if(i+j>=6):
+#             print("*",end=" ")
+#         else:
+#             print(" " ,end=" ")
+#     print()
+
+
 #      * 
 #     * *
 #    * * *
 #   * * * * 
 #  * * * * * 
 
+# for i in range(1,6):
+#     for j in range(1,6):
+#         if(i+j>=6):
+#             print("*",end=" ")
+#         else:
+#             print(" " ,end="")
+#     print()
