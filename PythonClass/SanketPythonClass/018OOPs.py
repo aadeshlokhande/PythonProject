24a4878d820c28977e980fac7f95113ee5cf6ea2
aadesh lokhande <aadeshlokhande11@gmail.com>
2022-09-28 08:44:50 +0530

    28-09-2022

---
+++ PythonClass/SanketPythonClass/018OOPs.py
@@ -0,0 +1,17 @@
+# OOPs - Object oriantated programming 
+
+# class 
+# object 
+    # constructor 
+
+# inheritance
+    # single inheritance
+    # multiple inheritance
+    # multilevel inheritance
+    # heirarchical inheritance
+    # hybrid inheritance
+# polymorphism
+    # function overloading
+    # fucntion overriding
+# abstraction
+# encapsulation
