03a0098a628ed28baf3618736819f5a421add670
aadesh lokhande <aadeshlokhande11@gmail.com>
2022-10-02 18:05:06 +0530

    02-10-2022

--- DataScience/Maths/multiplication/Multiplication.py
+++ DataScience/Maths/multiplication/Multiplication.py
@@ -1,6 +1,6 @@
 from random import randint
 import os 
-file = open("multiplication/multiSet1.txt","a")
+file = open("multiplication/multiSet2.txt","a")
 
 a = randint(11,99)
 b = randint(11,99)
@@ -9,5 +9,8 @@ os.system("clear")
 
 file.write(f"{a} x {b} = {a*b}\n")
 print(f"\n{a} x {b}\n")
+input()
+
+print(f"{a * b}\n")
 
 file.close()
