5a336d9fedabe193be2faf9709eef6908a5dc801
Aadesh Lokhande <aadeshlokhande11@gmail.com>
2022-12-07 10:10:03 +0530

    msg

--- ShortVideosCode/code/codeWrite.py
+++ ShortVideosCode/code/codeWrite.py
@@ -1,60 +1,31 @@
 import pyautogui as pg
 from time import sleep
 
-filename = "Palindrome.c"
+filename = "001MonthCalender.py"
+credit = "# Author : @Tech_In_Seconds\n"
 
-credit = "// Author : @Tech_In_Seconds\n"
-
-data = """#include<stdio.h>  
-int main()    
-{    
-    int n,r,sum=0,temp;    
-    printf("enter the number=");    
-    scanf("%d",&n);    
-    temp=n;    
-    while(n>0)    
-    {    
-        r=n%10;    
-        sum=(sum*10)+r;    
-        n=n/10;    
-    }    
-    if(temp==sum)
-    {
-        printf("palindrome number \\n");
-    }
-    else 
-    {
-        printf("not palindrome\\n");   
-    }
-    return 0;  
-}"""
+file = open(filename,"r")
+data = file.read()
+file.close()
 
 data = credit + data
-
-data = data.replace("    ","")
+data = data.split("\n")
 
 sleep(1)
 pg.click(400,300)
 sleep(3)
 pg.moveTo(1100,800)
 
-for i in data:
-    if(i=="<"):
-        pg.hotkey("ctrl","v")
-    elif(i=="{"):
-        pg.typewrite(i)
-        pg.press("del")
-    else:
-        pg.typewrite(i)
-
-sleep(1)
-
-
-pg.hotkey("alt","t")
-sleep(2)
-pg.typewrite(f"gcc {filename} && ./a.out",0.08)
-pg.press("Enter")
-sleep(1)
-
-pg.typewrite(f"12321",0.2)
-pg.press("Enter")
+for line in data:
+    for ch in line:
+        if(ch=="<"):
+            pg.hotkey("ctrl","v")
+        elif(ch=="{"):
+            pg.typewrite(ch)
+            pg.press("del")
+        else:
+            pg.typewrite(ch)
+    pg.typewrite("\n")
+    
+pg.hotkey("shift","r")
+pg.press("r")
